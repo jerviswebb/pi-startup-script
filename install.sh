7b64c7a865128f0b0f37fcd5db8875a6721b0b49
Anthony Ma <ama@jerviswebb.com>
2022-08-17 09:14:41 -0400

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Testing hello"
+while true; do
+    if [ $(systemctl is-active zerotier-one) = "active" ]; then
+        break
+    fi
+    sleep 1
+done
+
+sleep 10
+
+# Reset ZeroTier node id
+service zerotier-one stop
+rm /var/lib/zerotier-one/identity.*
+service zerotier-one start
